a1e774b46384f2f028643ba7d54da1e71fe780f8
MurgaNS <nemanja.murgaski@gmail.com>
2023-07-24 13:51:49 +0100

    Project initialization

---
+++ Backend/graduate-thesis-polovni-automobili/src/main/java/ftn/graduatethesispolovniautomobili/GraduateThesisPolovniAutomobiliApplication.java
@@ -0,0 +1,13 @@
+package ftn.graduatethesispolovniautomobili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateThesisPolovniAutomobiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduateThesisPolovniAutomobiliApplication.class, args);
+	}
+
+}
---
+++ Backend/graduate-thesis-polovni-automobili/src/test/java/ftn/graduatethesispolovniautomobili/GraduateThesisPolovniAutomobiliApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.graduatethesispolovniautomobili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateThesisPolovniAutomobiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
